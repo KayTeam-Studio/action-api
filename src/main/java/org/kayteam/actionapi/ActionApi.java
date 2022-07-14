78781efc0048f6761dcbcf856645cf84d141edeb
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-07-14 18:26:24 -0300

    ActionApi.java - Added static instance to use on minecraft servers as a dependency

--- src/main/java/org/kayteam/actionapi/ActionApi.java
+++ src/main/java/org/kayteam/actionapi/ActionApi.java
@@ -1,18 +1,32 @@
 package org.kayteam.actionapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.kayteam.actionapi.action.ActionManager;
+import org.kayteam.actionapi.utils.BrandSender;
 
 public final class ActionApi extends JavaPlugin {
 
+    private final ActionManager actionManager = new ActionManager();
+    private static ActionApi actionApi = null;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        actionApi = this;
+        actionManager.register();
+        BrandSender.sendVersionStatus(this, getServer().getConsoleSender(), "&aEnabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        BrandSender.sendVersionStatus(this, getServer().getConsoleSender(), "&cDisabled");
+    }
+
+    public ActionManager getActionManager() {
+        return actionManager;
+    }
+
+    public static ActionApi getInstance() {
+        return actionApi;
     }
 
 }
